a1fda202a99445f74c9dd3616effb3ba7d310b4a
budyadna <budyadnayahya@gmail.com>
2024-12-26 02:36:44 +0700

    Perbaikan HTML, CSS, dan Javascript

--- js/script.js
+++ js/script.js
@@ -1,33 +1,27 @@
-// Gallery
 const galleryContainer = document.querySelector(".gallery-container");
+const slides = document.querySelectorAll(".gallery-slide");
 const leftBtn = document.querySelector(".left-btn");
 const rightBtn = document.querySelector(".right-btn");
 
 let currentIndex = 0;
-const cardPerView = 6;
-const totalCards = galleryContainer.children.length;
-const totalSlides = math.cell(totalCards / cardPerView);
 
-// Update bottons
 const updateButtons = () => {
   leftBtn.classList.toggle("hidden", currentIndex === 0);
-  rightBtn.classList.toggle("hidden", currentIndex === totalSlides - 1);
+  rightBtn.classList.toggle("hidden", currentIndex === slides.length - 1);
 };
 
-// Jalankan fungsi left-btn
 leftBtn.addEventListener("click", () => {
   if (currentIndex > 0) {
     currentIndex--;
-    galleryContainer.style.transform = `translateY(-${currentIndex * 100}%)`;
+    galleryContainer.style.transform = `translateX(-${currentIndex * 100}%)`;
     updateButtons();
   }
 });
 
-// Jalankan fungsi right-btn
 rightBtn.addEventListener("click", () => {
-  if (currentIndex < totalSlides - 1) {
+  if (currentIndex < slides.length - 1) {
     currentIndex++;
-    galleryContainer.style.transform = `translateY(${currentIndex * 100}%)`;
+    galleryContainer.style.transform = `translateX(-${currentIndex * 100}%)`;
     updateButtons();
   }
 });
